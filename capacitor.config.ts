bd5208ecfb55df30058d0ccef3e3c8ef019a2ca8
MaximeGrmd <maxime.grimaud38@gmail.com>
2022-01-06 10:59:15 +0100

    Create a sidemenu ionic app

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Test Screeb',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
